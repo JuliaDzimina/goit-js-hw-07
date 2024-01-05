860dc03b4c8b18ce94b090b3970673a3d0d019f8
JuliaDzimina <132845748+JuliaDzimina@users.noreply.github.com>
2024-01-05 23:09:16 +0200

    task5

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const button = document.querySelector('.change-color');
+const spanColor = document.querySelector('.color')
+const bodyStule = document.querySelector('body')
+
+button.addEventListener('click', onButtonClick);
+
+function  onButtonClick(){
+  bodyStule.style.backgroundColor = getRandomHexColor();
+  spanColor.textContent = getRandomHexColor();
+}
