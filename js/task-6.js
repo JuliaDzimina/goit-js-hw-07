c2f1e47428dc57b78fb9df330854a35d5c93f996
JuliaDzimina <132845748+JuliaDzimina@users.noreply.github.com>
2024-01-06 20:18:01 +0200

    task6

--- js/task-6.js
+++ js/task-6.js
@@ -5,5 +5,41 @@ function getRandomHexColor() {
 }
 
 
+//Атрибути HTML
 const buttonCreate = document.querySelector('button[data-create]');
+const buttonDestroy = document.querySelector('button[data-destroy]');
+const inputNumber = document.querySelector('input');
+const boxes = document.querySelector('#boxes');
+
+// Події
+buttonCreate.addEventListener('click', createBoxes);
+buttonDestroy.addEventListener('click', destroyBoxes);
+
+// Функції
+
+function createBoxes() {
+ boxes.innerHTML = '';
+
+ const  amount = inputNumber.value;
+  if (amount < 1 || amount > 100) {
+    alert('Please enter a number between 1 and 100!')
+  }
+  
+  let sizeBox = 30;
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement('div');
+    box.style.width = `${sizeBox}px`;
+    box.style.height = `${sizeBox}px`;
+    sizeBox  += 10;
+  box.style.backgroundColor = getRandomHexColor();
+  boxes.appendChild(box);
+}
+inputNumber.value = '';
+};
+
+function destroyBoxes(){
+  boxes.innerHTML = '';
+  
+}
+
 
